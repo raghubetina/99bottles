fa04c765546d4a74c5cade8c4f279351cc2bdf5d
Raghu Betina <raghu@firstdraft.com>
2018-05-27 22:17:56 -0500

    Third test passes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,12 @@
+require "active_support/core_ext/string"
+
 class Bottles
   def verse(number_of_bottles)
+    next_number = number_of_bottles - 1
+
     <<-TEXT.gsub(/^\s+/, "")
       #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-      Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.
+      Take one down and pass it around, #{next_number} #{"bottle".pluralize(next_number)} of beer on the wall.
     TEXT
   end
 end
